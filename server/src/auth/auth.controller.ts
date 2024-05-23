907b17e7618c75d07e554f9110a72e094578420f
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-23 00:37:31 -0300

    feat: create auth controler

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('integrations/auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
