4f4f05acb474cf7274515ea61e55b0846d21483c
Eron Alves <eron.alves@letwe.com.br>
2024-05-23 10:54:10 -0300

    chore: install jwt pacakge

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 
 @Controller('integrations/auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
