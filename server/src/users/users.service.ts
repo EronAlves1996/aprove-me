5af3e324ebbae08e0b4cf9d9d6f595bc61edf433
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-23 00:29:40 -0300

    feat: change user service to retrieve fake user

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+type User = {
+  login: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      login: 'aprovame',
+      password: 'aprovame',
+    },
+  ];
+
+  async retrieve(_: string): Promise<User | undefined> {
+    return this.users[0];
+  }
+}
