7f41886738cfa565c86b1ae234338708e3428088
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-23 00:35:08 -0300

    feat: create sign in logic

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string) {
+    const user = await this.usersService.retrieve(username);
+    if (user.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...rest } = user;
+    return rest;
+  }
+}
