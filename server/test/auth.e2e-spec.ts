1165d13c2e9c611ffbfc49035837f4114a44cd08
Eron Alves <eron.alves@letwe.com.br>
2024-05-24 17:16:25 -0300

    test: update tests with complete auth flow

--- server/test/auth.e2e-spec.ts
+++ server/test/auth.e2e-spec.ts
@@ -4,14 +4,39 @@ import { AppModule } from '../src/app.module';
 import { AssignorService } from 'src/assignor/assignor.service';
 import { randomUUID } from 'crypto';
 import * as request from 'supertest';
+import { UserDto } from 'src/auth/UserDto';
+import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('Auth', () => {
   let app: INestApplication;
+
   const entity = {
     id: randomUUID(),
   };
+
   const assignorService = {
     retrieve: (_: string) => entity,
+  };
+
+  const prismaUserService = () => {
+    const users = new Map();
+
+    return {
+      user: {
+        create: async (data: {data:UserDto}) => {
+          users.set(data.data.login, data.data);
+          return data;
+        },
+        findFirst: async (where: { where: { login: string } }) => {
+          return users.get(where.where.login);
+        },
+        count: async (where: {login: string}) => {
+          if (users.has(where.login)) return 1;
+          return 0;
+        },
+      },
+    };
   };
 
   beforeAll(async () => {
@@ -20,7 +45,10 @@ describe('Auth', () => {
     })
       .overrideProvider(AssignorService)
       .useValue(assignorService)
+      .overrideProvider(PrismaService)
+      .useValue(prismaUserService())
       .compile();
+
     app = moduleRef.createNestApplication();
     await app.init();
   });
@@ -33,6 +61,11 @@ describe('Auth', () => {
 
   it('Authorization flow', async () => {
     const client = request(app.getHttpServer());
+    const registerResponse = await client
+      .post('/integrations/auth/register')
+      .set('Content-Type', 'application/json')
+      .send({ login: 'aprovame', password: 'aprovame' });
+    expect(registerResponse.statusCode).toBe(HttpStatus.CREATED);
     const { body, statusCode } = await client
       .post('/integrations/auth')
       .send({ login: 'aprovame', password: 'aprovame' })
